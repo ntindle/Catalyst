0a92ea0340f9d0aaa48bcacfe8e0e31d0ce0e8a2
jasonmally <jasonmally1@gmail.com>
2019-03-12 15:10:16 -0700

    Added initial Unity to MK3 MIDI translate script.

---
+++ midi_translate.py
@@ -0,0 +1,20 @@
+# MIDIHandler adapted from pymidi:
+# https://pypi.org/project/pymidi/#description
+
+from pymidi import server
+
+unity_to_mk3 = {} #we will populated this with the mapping of Unity Key IDs to MK3 keys
+
+class MIDIHandler(server.handler)
+    def on_peer_connected(self, peer):
+        print('Peer connected: {}'.format(peer))
+
+    def on_peer_disconnected(self, peer):
+        print('Peer disconnected: {}'.format(peer))
+
+    def on_midi_commands(self, command_list):
+        for command in command_list:
+            if command.command == 'note_on':
+                key = command.params.key
+                velocity = command.params.velocity
+                print('Someone hit the key {} with velocity {}'.format(key, velocity)
