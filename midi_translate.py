638cde4f4ac62937031b8e7a5e980e1d51bbf68d
jasonmally <jasonmally1@gmail.com>
2019-03-12 19:34:00 -0700

    Added functions to translate Unity MIDI message into format readable for Maschine MK3

--- midi_translate.py
+++ midi_translate.py
@@ -16,15 +16,67 @@ portname = None
 # A pentatonic scale
 notes = [60, 62, 64, 67, 69, 72]
 
-def parseInputString(s):
-	
+def noteToInt(note):
+	note_value = 0
+	length = len(note)
+	if(note[length - 2] != "-"):
+		octave = note[length - 1] + 1
+		note_value += 12 * octave
+	if(note[0] == 'C'):
+		if(note[1] == '#'):
+			note_value += 1
+	elif(note[0] == 'D'):
+		if(note[1] == '#'):
+			note_value += 3
+		else:
+			note_value += 2
+	elif(note[0] == 'E'):
+		note_value += 4
+	elif(note[0] == 'F'):
+		if(note[1] == '#'):
+			note_value += 6
+		else:
+			note_value += 5
+	elif(note[0] == 'G'):
+		if(note[1] == '#'):
+			note_value += 8
+		else:
+			note_value += 7
+	elif(note[0] == 'A'):
+		if(note[1] == '#'):
+			note_value += 10
+		else:
+			note_value += 9
+	else: #note is B
+		note_value += 11
+	return note_value
+
+def getMapping(unity_note):
+	mk3_note = unity_note
+	return mk3_note
+
+def parseUnityString(in_str):
+	components = in_str.split(", ")
+	mk3_msg = components[2]
+	chnl = components[3]
+	note_str = components[4]
+	note_velocity = note_str.split()
+	unity_note = noteToInt(note_velocity[0])
+	mk3_note = getMapping(unity_note)
+	vel = int(note_velocity[1])
+	msg = Message(mk3_msg, note=mk3_note, velocity=vel, channel=chnl)
+	return msg
 
 def main():
-	if len(sys.argv) > 1:
-    	portname = sys.argv[1]
+	if len(sys.argv) < 2:
+    	print("Missing input string")
+    	exit(1)
 	else:
-    	portname = None
-	generateRandomNotes()
+    	unity_msg = sys.argv[1]
+    	mk3_msg = parseUnityString(unity_msg)
+		with mido.open_output(portname, autoreset=True) as port:
+		    print('Using {}'.format(port)) 
+	        port.send(mk3_msg)
 
 if __name__ == '__main__':
 	main()
